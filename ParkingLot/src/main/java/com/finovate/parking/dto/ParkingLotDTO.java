b9126e8ff103306c1b54aa6ecfe62c111313cf75
Bhushan Patil <bhupatil0001@gmail.com>
2021-05-09 00:50:59 +0530

    Added: parkingLot DTO for easy to pass object with secure

---
+++ ParkingLot/src/main/java/com/finovate/parking/dto/ParkingLotDTO.java
@@ -0,0 +1,8 @@
+package com.finovate.parking.dto;
+import lombok.Data;
+
+@Data
+public class ParkingLotDTO {		//data transfer object for parkinglot car
+	public Integer lotId;
+	public String parkingLotName;
+}
